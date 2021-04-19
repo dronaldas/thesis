deba26b99929c00a6c3c45bab964cde7da777cdc
Arnoldas Mikulevicius <barttuxas@gmail.com>
2021-04-19 21:37:43 +0300

    included slider database

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// swiper gallery
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    autoHeight: true,
+   
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  });
+  // swiper gallery ends here
