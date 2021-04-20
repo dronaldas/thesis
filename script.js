2cd96126cfdafb57bce2d8c33d4b8f767e2b02f2
Arnoldas Mikulevicius <barttuxas@gmail.com>
2021-04-20 23:23:27 +0300

    tabs added

--- script.js
+++ script.js
@@ -13,3 +13,33 @@ const swiper = new Swiper('.swiper-container', {
     },
   });
   // swiper gallery ends here
+
+  // Tabs section
+let tabNavigationBlocks = document.querySelectorAll('.tab_section nav ul li');
+let tabContentBlocks = document.querySelectorAll('.tab_section .tab-content');
+
+function removeActiveNavigation() {
+  for (let tab of tabNavigationBlocks) {
+    tab.classList.remove('active');
+  }
+}
+
+function removeActiveTabs() {
+  for (let tab of tabContentBlocks) {
+    tab.classList.remove('active');
+  }
+}
+
+for (let tab of tabNavigationBlocks) {
+  tab.addEventListener('click', function() {
+    removeActiveNavigation();
+    removeActiveTabs();
+
+    this.classList.add('active');
+
+    let index = this.dataset.slide;
+
+    document.querySelector('.tab_section .tab-content[data-slide="'+index+'"]').classList.add('active');
+  })
+}
+// tabs section ends
